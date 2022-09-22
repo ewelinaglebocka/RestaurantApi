0eaf3dfff72beaed6c985ae7d783773e651cf463
Ewelina Głębocka <ewelina.glebocka@mardom.com.pl>
2022-09-22 15:43:17 +0200

    Refactor finaly

--- RestaurantApi/Entities/User.cs
+++ RestaurantApi/Entities/User.cs
@@ -6,7 +6,7 @@
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateTime? DataOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
         public string Nationality { get; set; }
         public string PasswordHash { get; set; }
         public int RoleId { get; set; }
